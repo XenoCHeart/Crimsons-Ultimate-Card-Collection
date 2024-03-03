ab97b8d0625ff93fcefbae144a01f8ba8d6435c6
Crimson Heart <71236338+XenoCHeart@users.noreply.github.com>
2024-03-03 13:12:58 -0500

    Add files via upload

---
+++ PlainDeck/PlainDeck.lua
@@ -0,0 +1,29 @@
+--- STEAMODDED HEADER
+--- MOD_NAME: Crimson's Pain Deck
+--- MOD_ID: PlainDeck
+--- MOD_AUTHOR: [Crimson Heart]
+--- MOD_DESCRIPTION: Includes a deck with no special effects. 
+
+----------------------------------------------
+------------MOD CODE -------------------------
+
+function SMODS.INIT.PlainDeck()
+local ch_plain_art = SMODS.findModByID("PlainDeck")
+
+local sprite_centers = SMODS.Sprite:new("centers", ch_plain_art.path, "Enhancers.png", 71, 95, "asset_atli")
+sprite_centers:register()
+
+
+local loc_def = {
+    ["name"] = "Plain Deck",
+    ["text"] = {
+        [1] = "Base Deck",    
+    },
+
+}
+
+local PlainDeck = SMODS.Deck:new("Plain Deck", "ch_plain", {my_deck_effect = true}, {x = 0, y = 5}, loc_def)
+PlainDeck:register()
+end
+----------------------------------------------
+------------MOD CODE END----------------------
